753212bd688c002eda67712376fffee5ba12bbe4
yous <62238900+yau33@users.noreply.github.com>
2022-10-19 17:11:32 +0800

    project redone
    no css

--- app.js
+++ app.js
@@ -1,74 +1,151 @@
-//a function that will randomly an option among 'Rock', 'Paper' and 'Scissors'
-let playerSelection;
-let computerSelection;
+const roundDisplay = document.createElement('h1');
+const playerDisplay = document.createElement('h1');
+const playerScoreDisplay = document.createElement('h1');
+const computerDisplay = document.createElement('h1');
+const computerScoreDisplay = document.createElement('h1');
+const resultDisplay = document.createElement('h1');
+const battleGrid = document.querySelector('.battle');
+const startAgainButton = document.createElement('button');
+battleGrid.append(roundDisplay,playerDisplay,playerScoreDisplay,computerDisplay,computerScoreDisplay,resultDisplay);
+
+let round = 1;
 let playerScore = 0;
 let computerScore = 0;
+roundDisplay.innerHTML = 'Round '+round;
+playerDisplay.innerHTML = 'Player: ';
+playerScoreDisplay.innerHTML = 'Player Score: '+ playerScore;
+computerDisplay.innerHTML = 'Computer: ';
+computerScoreDisplay.innerHTML = 'Computer Score: '+ computerScore;
+resultDisplay.innerHTML = 'Result: ';
+startAgainButton.innerHTML = 'Start Again';
+resultDisplay.after(startAgainButton);
+
+const choices = [
+    {
+        name: 'rock',
+        emoji: '✊',
+        beats: 'scissors'
+    },
+    {
+        name: 'paper',
+        emoji: '✋',
+        beats: 'rock'
+    },
+    {
+        name: 'scissors',
+        emoji: '✌',
+        beats: 'paper'
+    }
+]
+
+
+choices.forEach(choice => {
+    const playerSelection = choice.name;
+    const button = document.createElement('button');
+    button.innerHTML = playerSelection;
+    roundDisplay.after(button);
+    button.addEventListener('click', e=>{
+        if(round<=5){
+            roundDisplay.innerHTML = 'Round '+round;
+            round++;
+
+            playerDisplay.innerHTML = 'Player: '+playerSelection;
+            let computerSelection = getComputerChoice();
+            displayComputerChoice(computerSelection);
+
+            if(playerWins(playerSelection,computerSelection)){
+                playerScore++;
+                playerScoreDisplay.innerHTML = 'Player Score: '+ playerScore;
+            }
+
+            if(computerWins(playerSelection,computerSelection)){
+                computerScore++;
+                computerScoreDisplay.innerHTML = 'Computer Score: '+ computerScore;
+            }
+
+        }else{
+            disableButton();
+            result();
+            setAllTextGray();
+            startAgain();
+        }
+    })
+});
+
+function setAllTextGray(){
+    const allTexts = document.querySelectorAll('h1');
+    allTexts.forEach(text =>{
+        text.style.opacity = '0.3';
+    })
+}
+
+function startAgain(){
+    startAgainButton.addEventListener('click', ()=>{
+        round = 1;
+        playerScore = 0;
+        computerScore = 0;
+        roundDisplay.innerHTML = 'Round '+round;
+        playerDisplay.innerHTML = 'Player: ';
+        playerScoreDisplay.innerHTML = 'Player Score: '+ playerScore;
+        computerDisplay.innerHTML = 'Computer: ';
+        computerScoreDisplay.innerHTML = 'Computer Score: '+ computerScore;
+        resultDisplay.innerHTML = 'Result: ';
+        activateButton();
+        activateTexts();
+    })
+    
+}
+
+function activateButton(){
+    const allButtons = document.querySelectorAll('button');
+    for (let i = 0; i< 3; i++){
+        allButtons[i].disabled =false;
+    }
+}
+
+function activateTexts(){
+    const allTexts = document.querySelectorAll('h1');
+    allTexts.forEach(text =>{
+        text.style.opacity = '1';
+    })
+}
+
+function disableButton(){
+    const allButtons = document.querySelectorAll('button');
+    for (let i = 0; i< 3; i++){
+        allButtons[i].disabled =true;
+    }
+}
+
+function result(){
+    if(playerScore>computerScore){
+        resultDisplay.innerHTML = 'Result: Player WINS !';
+    }
+    if(playerScore<computerScore){
+        resultDisplay.innerHTML = 'Result: Computer WINS !';
+    }
+    if(playerScore === computerScore){
+        resultDisplay.innerHTML = 'Result: Tied.';
+    }
+}
 
 function getComputerChoice(){
-    let arr = ['Rock', 'Paper', 'Scissors'];
-    let randNum = Math.floor(Math.random()*3);
-    return arr[randNum];
+    let computerChocie = choices[Math.floor(Math.random()*choices.length)].name;
+    return computerChocie;
 }
 
-function playRound(playerSelection, computerSelection){
-    
-    let userInput = prompt('1.Rock 2.Paper 3.Scissors. Enter number.');
-    computerSelection = getComputerChoice();
-
-    if(userInput === "1"){
-        playerSelection = "Rock";
-    }
-    if(userInput === "2"){
-        playerSelection = "Paper";
-    }
-    if(userInput === "3"){
-        playerSelection = "Scissors";
-    }
-
-    if(playerSelection === computerSelection){
-        return `Tied.
-        Player : Computer = ${playerSelection} : ${computerSelection} = ${playerScore}:${computerScore}`;
-    }
-
-    if(playerSelection ==='Rock' && computerSelection === 'Paper'){
-        computerScore+=1;
-        return `You lose.
-        Player : Computer = ${playerSelection} : ${computerSelection} = ${playerScore}:${computerScore}`;
-    }
-    if(playerSelection ==='Rock' && computerSelection === 'Scissors'){
-        playerScore+=1;
-        return `You win.
-        Player : Computer = ${playerSelection} : ${computerSelection} = ${playerScore}:${computerScore}`;
-    }
-
-    if(playerSelection ==='Paper' && computerSelection === 'Scissors'){
-        computerScore+=1;
-        return `You lose.
-        Player : Computer = ${playerSelection} : ${computerSelection} = ${playerScore}:${computerScore}`;
-    }
-    if(playerSelection ==='Paper' && computerSelection === 'Rock'){
-        playerScore+=1;
-        return `You win.
-        Player : Computer = ${playerSelection} : ${computerSelection} = ${playerScore}:${computerScore}`;
-    }
-
-    if(playerSelection ==='Scissors' && computerSelection === 'Rock'){
-        computerScore+=1;
-        return `You lose.
-        Player : Computer = ${playerSelection} : ${computerSelection} = ${playerScore}:${computerScore}`;
-    }
-    if(playerSelection ==='Scissors' && computerSelection === 'Paper'){
-        playerScore+=1;
-        return `You win.
-        Player : Computer = ${playerSelection} : ${computerSelection} = ${playerScore}:${computerScore}`;
-    }
+function displayComputerChoice(computerSelection){
+    computerDisplay.innerHTML = 'Computer: '+ computerSelection;
 }
 
-function game(){
-    for(let i=1;i<6;i++){
-        console.log(playRound(playerSelection, computerSelection));
-    }
-    
+function playerWins(player, opponent){
+    const found = choices.find(choice => choice.name === player);
+    return found.beats === opponent;
 }
 
-game();
+function computerWins(player, opponent){
+    const found = choices.find(choice => choice.name === opponent);
+    return found.beats === player;
+}
+
+
